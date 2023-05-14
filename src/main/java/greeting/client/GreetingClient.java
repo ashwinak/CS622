d9b50a0f68cf216fa109569b21ad795f3aa4bdd0
ashwinak <ashwinak@gmail.com>
2023-05-14 16:26:36 -0700

    first commit

---
+++ src/main/java/greeting/client/GreetingClient.java
@@ -0,0 +1,50 @@
+package greeting.client;
+
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    private static void doGreet(ManagedChannel channel) {
+        System.out.println("Enter doGreet");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("Ashwin").build());
+        System.out.println("Greeting: " + response.getResult());
+    }
+
+    private static void doGreetManyTimes(ManagedChannel channel) {
+        System.out.println("Enter doGreetManyTimes");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        stub.greetManyTimes(GreetingRequest.newBuilder().setFirstName("Ashwin").build()).forEachRemaining(response -> {
+            System.out.println(response.getResult());
+        });
+    }
+
+    public static void main(String[] args) {
+
+        if(args.length ==0) {
+            System.out.println("Need one argument to work");
+            return;
+        }
+
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost",50051)
+                .usePlaintext()
+                .build();
+
+        switch (args[0]) {
+            case "greet" : doGreet(channel);break;
+            case "greet_many_times": doGreetManyTimes(channel); break;
+            default:
+                System.out.println("Keyword Invalid: " + args[0]);
+        }
+
+        System.out.println("Shutting Down");
+        channel.shutdown();
+    }
+
+}
